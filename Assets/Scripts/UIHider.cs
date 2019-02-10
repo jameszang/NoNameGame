d326f932ef6ccd634a0dee298ed0d37eb6b1d656
James Zang <jameslzang@hotmail.com>
2019-02-10 08:27:20 -0800

    Fix supermarket rendering bug

---
+++ Assets/Scripts/UIHider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHider : MonoBehaviour
+{
+    UnityEngine.UI.Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Supermarket") {
+            img.color = new Color(255, 255, 255, 0);
+        } else {
+            img.color = new Color(255, 255, 255, 255);
+        }
+    }
+}
