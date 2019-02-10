fe1b06390950de2a1b4d016ea9f62ab479d3d808
leaf1tree <47487750+leaf1tree@users.noreply.github.com>
2019-02-09 17:31:01 -0800

    Changed logic for sliding activities menu

--- Assets/Scripts/ActivitiesScript.cs
+++ Assets/Scripts/ActivitiesScript.cs
@@ -4,38 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ActivitiesScript : MonoBehaviour {
-    
+
+
     public void ChangeScene(string scene) {
         SceneManager.LoadScene(scene);
     }
 
-    public void SlideOut(GameObject obj1, GameObject obj2, GameObject obj3, GameObject obj4)
-    {
-        ArrayList Activities = new ArrayList();
-        Activities.Add(obj1);
-        Activities.Add(obj2);
-        Activities.Add(obj3);
-        Activities.Add(obj4); 
+    public void SlideOut(List<GameObject> Activities)
+    { 
+
         foreach (GameObject gameObject in Activities)
         {
             gameObject.transform.Translate(Vector3.right * 800);
         }
         
     }
-
-    public void ChanghgjhgSe(GameObject obj1, GameObject obj2, GameObject obj3, GameObject obj4)
-    {
-        ArrayList Activities = new ArrayList();
-        Activities.Add(obj1);
-        Activities.Add(obj2);
-        Activities.Add(obj3);
-        Activities.Add(obj4);
-
-        foreach (GameObject gameObject in Activities)
-        {
-            gameObject.transform.Translate(Vector3.right * 800);
-        }
-    }
-
-
 }
