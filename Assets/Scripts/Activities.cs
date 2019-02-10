3a7d8ddd1c87f7a06f337e249ddaa83c9cd6f507
leaf1tree <47487750+leaf1tree@users.noreply.github.com>
2019-02-10 03:00:06 -0800

    Hospital Button

---
+++ Assets/Scripts/Activities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities : MonoBehaviour
+{
+    private Animator animator;
+    private bool isInside;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isInside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActivitiesSlideLeft()
+    {
+        if (isInside)
+        {
+            //FridgeSlideLeft
+            animator.Play("ActivitiesSlideRight");
+        }
+        else
+        {
+            animator.Play("ActivitiesSlideLeft");
+        }
+        isInside = !isInside;
+    }
+}
