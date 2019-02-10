2bfc907c6468c81d262984b7af5653731b04060c
leaf1tree <47487750+leaf1tree@users.noreply.github.com>
2019-02-10 01:22:24 -0800

    Shopping Interface

---
+++ Assets/Scripts/Fridge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fridge : MonoBehaviour {
+    private Animator animator;
+    private bool isInside;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isInside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FridgeSlideRight()
+    {
+        if (isInside)
+        {
+            //FridgeSlideLeft
+            animator.Play("FridgeSlideLeft");
+        }
+        else
+        {
+            animator.Play("FridgeSlideRight");
+        }
+        isInside = !isInside;
+    }
+}
