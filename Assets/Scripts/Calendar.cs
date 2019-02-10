c5df704a7893dfd6c441af69ec050e973b0615cc
James Zang <jameslzang@hotmail.com>
2019-02-10 14:21:22 -0800

    Implement calendar + remove gym placeholder

---
+++ Assets/Scripts/Calendar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour
+{
+
+    public static int day = 0;
+    private static UnityEngine.UI.Text text;
+
+    private void Awake() {
+        text = GetComponent<UnityEngine.UI.Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            day++;
+            if (day > 31) {
+                day = 1;
+            }
+            text.text = day.ToString();
+        }
+    }
+}
