296254a9ab55ba0f25ac5126912ae3b70e6e89c5
Shiole <kitty98low@gmail.com>
2019-02-10 04:03:12 -0800

    Made pull out menus global

---
+++ Assets/Scripts/ProductFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductFade : MonoBehaviour
+{
+    private Animator animator;
+    private bool isClicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowProductInfo()
+    {
+        if (isClicked)
+        {
+            animator.Play("ShowProduct");
+        }
+    }
+}
