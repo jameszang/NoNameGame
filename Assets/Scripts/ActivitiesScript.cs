6b1c728c4f25b2b3c268a57c6921216afcd7f58a
leaf1tree <47487750+leaf1tree@users.noreply.github.com>
2019-02-09 17:11:26 -0800

    Wrote logic for sliding activities menu

--- Assets/Scripts/ActivitiesScript.cs
+++ Assets/Scripts/ActivitiesScript.cs
@@ -8,4 +8,34 @@ public class ActivitiesScript : MonoBehaviour {
     public void ChangeScene(string scene) {
         SceneManager.LoadScene(scene);
     }
+
+    public void SlideOut(GameObject obj1, GameObject obj2, GameObject obj3, GameObject obj4)
+    {
+        ArrayList Activities = new ArrayList();
+        Activities.Add(obj1);
+        Activities.Add(obj2);
+        Activities.Add(obj3);
+        Activities.Add(obj4); 
+        foreach (GameObject gameObject in Activities)
+        {
+            gameObject.transform.Translate(Vector3.right * 800);
+        }
+        
+    }
+
+    public void ChanghgjhgSe(GameObject obj1, GameObject obj2, GameObject obj3, GameObject obj4)
+    {
+        ArrayList Activities = new ArrayList();
+        Activities.Add(obj1);
+        Activities.Add(obj2);
+        Activities.Add(obj3);
+        Activities.Add(obj4);
+
+        foreach (GameObject gameObject in Activities)
+        {
+            gameObject.transform.Translate(Vector3.right * 800);
+        }
+    }
+
+
 }
