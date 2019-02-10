ca1e491937e47996ed18b6f8c01a1f245c28a02f
leaf1tree <47487750+leaf1tree@users.noreply.github.com>
2019-02-10 06:18:00 -0800

    Textbox Toggle

---
+++ Assets/Scripts/Textbox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Textbox : MonoBehaviour
+{
+    public GameObject textbox;
+    private bool display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = false;
+        textbox.SetActive(false); 
+    }
+
+ 
+    public void ToggleTextbox()
+    {
+        if (display == false)
+        {
+            textbox.SetActive(true);
+            display = true; 
+        }
+        else  {
+            textbox.SetActive(false);
+            display = false; 
+        }
+        
+    }
+}
