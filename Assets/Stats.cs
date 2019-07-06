f1c1489a226208f0267638f2894f74040382fb0f
James Zang <jameslzang@hotmail.com>
2019-07-05 23:33:28 -0700

    Clean up Stats script

--- Assets/Stats.cs
+++ Assets/Stats.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatsNutrition : MonoBehaviour
+public class Stats : MonoBehaviour
 {
     private Animator animator;
     private bool isInside;
